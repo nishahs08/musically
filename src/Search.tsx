ab77d1e77c5f7ddfb560433b2a1cf262603eed78
nishahs08 <nishahs08@outlook.in>
2021-11-12 17:24:51 +0530

    refactor: Search component featured with search and select functionality

--- src/Search.tsx
+++ src/Search.tsx
@@ -1,11 +1,52 @@
 import { ReactJSXElement } from "@emotion/react/types/jsx-namespace";
-import { TextField } from "@mui/material";
-import { useState } from "react";
+import { Input, MenuItem, Select, TextField, Autocomplete, CircularProgress } from "@mui/material";
+import React, { useState } from "react";
+import { SearchedArtist } from "./types";
 
 interface SearchProps {
     searchString: string,
     onSearchStringChanged: (searchString: string) => void
+    matchedArtists: {
+        mbid: string
+        name: string
+    }[]
+    onArtistSelected: (selectedArtistMBID: string | null) => void
 }
-export const Search: React.FC<SearchProps> = ({ searchString, onSearchStringChanged }) => {
-    return <TextField name='search' placeholder='Search for artist' value={searchString} onChange={(e) => onSearchStringChanged(e.target.value)} />
+export const Search: React.FC<SearchProps> = ({ searchString, onSearchStringChanged, matchedArtists, onArtistSelected }) => {
+    const [open, setOpen] = useState(false);
+    return (
+        <Autocomplete
+            fullWidth
+            id="search"
+            open={open}
+            onOpen={() => {
+                setOpen(true);
+            }}
+            onClose={() => {
+                setOpen(false);
+            }}
+            onChange={(e, value) => {
+                onArtistSelected(value?.mbid || null);
+            }}
+            isOptionEqualToValue={(option, value) => option.mbid === value.mbid}
+            getOptionLabel={(option) => option.name}
+            options={matchedArtists}
+            renderInput={(params) => (
+                <TextField
+                    {...params}
+                    label="Search for artists"
+                    value={searchString}
+                    onChange={(e) => onSearchStringChanged(e.target.value)}
+                    InputProps={{
+                        ...params.InputProps,
+                        endAdornment: (
+                            <React.Fragment>
+                                {params.InputProps.endAdornment}
+                            </React.Fragment>
+                        ),
+                    }}
+                />
+            )}
+        />
+    )
 }
