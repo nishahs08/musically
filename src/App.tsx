966bb24473d896f70ed58bf30b47646b7cf3aeee
nishahs08 <nishahs08@outlook.in>
2021-11-12 17:25:37 +0530

    feature: App integrated with image grid and search component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,77 @@
+
 import { Container, Grid } from "@mui/material";
-import { useState } from "react";
-import { Search } from './Search'
+import { useEffect, useState } from "react";
+import { Search } from './Search';
+//@ts-ignore
+import FbImageLibrary from 'react-fb-image-grid';
+import { BrowseAlbumsResponse, ReleaseGroup, SearchedArtist } from "./types";
+import { findAlbums, searchArtists } from "./gql.service";
+import { useDebouce } from "./hooks";
+
+function thumbnailFromAudioDB(releaseGroup: ReleaseGroup) {
+	const { theAudioDB } = releaseGroup;
+	return theAudioDB?.backImage
+		|| theAudioDB?.frontImage;
+}
+
+function thumbnailFromFanArt(releaseGroup: ReleaseGroup) {
+	const { fanArt } = releaseGroup;
+	return (fanArt?.albumCovers || []).find((album) => album.url)?.url;
+}
+
+function thumnailURLForAlbum(album: ReleaseGroup) {
+	return thumbnailFromAudioDB(album)
+		|| thumbnailFromFanArt(album)
+		|| 'picture';
+}
+
 function App() {
 	const [searchString, setSearchString] = useState('');
+	const debouncedSearchString = useDebouce(searchString, 500);
+
+	const [matchedArtists, setMatchedArtists] = useState<SearchedArtist[]>([]);
+	useEffect(() => {
+		async function loadMatchingArtists() {
+			setMatchedArtists(await searchArtists(searchString));
+		}
+
+		if (debouncedSearchString !== '') {
+			loadMatchingArtists();
+		} else {
+			setMatchedArtists([]);
+		}
+	}, [debouncedSearchString]);
+
+	const [selectedArtistMBID, setSelectedArtistMBID] = useState<string | null>(null);
+	const [albums, setAlbums] = useState<ReleaseGroup[]>([]);
+	useEffect(() => {
+		async function loadAlbums(mbid: string) {
+			setAlbums(await findAlbums(mbid))
+		}
+
+		if (selectedArtistMBID) {
+			loadAlbums(selectedArtistMBID);
+		} else {
+			setAlbums([]);
+		}
+	}, [selectedArtistMBID]);
+
+	const imagesForImageGrid = albums.map((album) => thumnailURLForAlbum(album));
+	console.log(imagesForImageGrid);
+
 	return (
 		<Container maxWidth='lg' >
-			<Grid container justifyContent='center'>
-				<Grid item>
-					<Search searchString={searchString} onSearchStringChanged={setSearchString} />
+			<Grid container justifyContent='center' sx={{ marginTop: '100px' }}>
+				<Grid sm={12}>
+					<Search
+						matchedArtists={matchedArtists}
+						searchString={searchString}
+						onSearchStringChanged={setSearchString}
+						onArtistSelected={(mbid) => setSelectedArtistMBID(mbid)}
+					/>
+				</Grid>
+				<Grid sm={12}>
+					<FbImageLibrary images={imagesForImageGrid} />
 				</Grid>
 			</Grid>
 		</Container>
--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,41 @@
-export interface ArtistQueryResponse {
+export interface SearchedArtist {
+	name: string;
+	mbid: string;
+	fanArt: {
+		thumbnails: { url: string }[];
+	};
+}
+export interface SearchArtistResponse {
 	data: {
-		search: {
-			artists: {
-				nodes: {
-					name: string;
-					mbid: string;
-					fanArt: {
-						thumbnails: { url: string }[];
-					};
-				}[];
+		search?: {
+			artists?: {
+				nodes: SearchedArtist[];
+			};
+		};
+	};
+}
+
+export interface ReleaseGroup {
+	mbid: string;
+	title: string;
+	fanArt: {
+		albumCovers: {
+			url: string;
+		}[];
+	};
+	theAudioDB: {
+		frontImage: string | null;
+		backImage: string | null;
+		spineImage: string | null;
+		discImage: string | null;
+	} | null;
+}
+
+export interface BrowseAlbumsResponse {
+	data: {
+		browse?: {
+			releaseGroups: {
+				nodes: ReleaseGroup[];
 			};
 		};
 	};
